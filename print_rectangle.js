6d26c23736e75070bd0e9e150fca7289baef21b7
ijaolahawwalolamilekan <ijaolahawwal@gmaol.com>
2024-11-05 21:25:58 -0800

    shapes

---
+++ print_rectangle.js
@@ -0,0 +1,10 @@
+// loop in a loop
+console.log('Print rectangle')
+for (let c = 0; c < 4; c++) {
+    let display = ""
+    for (let v = 0; v < 7; v++) {
+        display = display + " #"
+    }
+    console.log(display)
+}
+    // display = "#####"
---
+++ print_triangle.js
@@ -0,0 +1,8 @@
+    console.log('Print triangle')
+    let display = ""
+    for (let d = 0; d < 10; d++) {
+        for (let x = 0; x < 1; x++) {
+            display  =   "#" + display 
+        }
+        console.log(display)
+    }
